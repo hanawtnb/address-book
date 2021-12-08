d055aa662375699327b6029f854697f52543aab0
hanawtnb <hanawtnb@icloud.com>
2021-12-08 22:16:10 +0900

    Firebaseのインストール

---
+++ src/config/firebase-config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDiazpKMoHFx9SjQR6YmbXShLjfHz45v7s",
+  authDomain: "address-book-2bcd7.firebaseapp.com",
+  projectId: "address-book-2bcd7",
+  storageBucket: "address-book-2bcd7.appspot.com",
+  messagingSenderId: "835257384290",
+  appId: "1:835257384290:web:9c35f140f4a5886ed4c03b",
+  measurementId: "G-9LP82DBT55",
+};
